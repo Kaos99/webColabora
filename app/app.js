f3c82c17128e62a0cc26b53d6ffdee80e69cf526
Kaos\Edward <esosoriocrispin@gmail.com>
2017-01-31 19:53:06 +0100

    Update function config: app.js

--- app/app.js
+++ app/app.js
@@ -4,5 +4,26 @@
 	angular
 		.module('app', ['ui.router'])
 		.config(config)
-		.run(run)
+		.run(run);
+
+	function config($stateProvider, $urlRouterProvider) {
+		//definimos la ruta por defecto
+		$urlRouterProvider.otherwise("/");
+
+		stateProvider
+			//estado para la pagina de inicio del usuario
+			.state('home', {
+				url: '/'
+				templateUrl: 'home/index.html',
+				controller: ''
+
+			})
+
+			//estado para la pagina de la cuenta del usuario
+			.state('useraccount',{
+
+			});
+	}
+
+
 })();
