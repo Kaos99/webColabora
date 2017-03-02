8cccedc957338e394c7c557a2ce24e0c4069c110
UB_EDU\edu.osorio <esosoriocrispin@gmail.com>
2017-03-02 14:01:42 +0100

    Creacion del servicio que muestra los mensajes flash en las paginas

---
+++ app/app-services/flash.service.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+    
+    angular
+        .module('app')
+        .factory('FlashService', Service);
+    
+    function Service($rootScope) {
+        var service = {};
+    }
+    
+    
+} 
+
+
+
+)
