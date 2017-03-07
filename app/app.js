511a6147a2bc26a84f134052914403a389b0cf70
Edward Steven Osorio Crispin <esosoriocrispin@gmail.com>
2017-03-07 22:58:13 +0100

    Actualización del fichero app.js. Adicionando funciones run

--- app/app.js
+++ app/app.js
@@ -10,29 +10,47 @@
 		//definimos la ruta por defecto
 		$urlRouterProvider.otherwise("/");
 
-		stateProvider
+		$stateProvider
 			//estado para la pagina de inicio del usuario
 			.state('home', {
 				url: '/',
 				templateUrl: 'home/index.html',
-				controller: '',
-				controllerAs:
+				controller: 'Home.IndexController',
+				controllerAs: 'vm',
+                data: { activeTab: 'home'}
 
 			})
 
 			//estado para la pagina de la cuenta del usuario
-			.state('useraccount', {
-				url: '/useraccount',
-				templateUrl: 'useraccount/index.html',
-				controller: '',
-				controllerAs: ''
+			.state('account', {
+				url: '/account',
+				templateUrl: 'account/index.html',
+				controller: 'Account.IndexController',
+				controllerAs: 'vm',
+                data: { activeTab: 'account'}
 
 			});
 	}
 
-	function run($http, $window) {
-		
+	function run($http, $rootScope ,$window) {
+        //agregamos como cabecera de autorizacion el token JWT
+        $http.defaults.headers.common['Authorization'] = 'Portador' + '$window.jwtToken';
+        
+        //actualizacion del estado en la barra de usuario
+        $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+		  $rootScope.activeTab = toState.data.activeTab;
+        });
 	}
-
-
+    
+    //activacion manual de angular boostrap desde que el token haya sido recibido desde el servidor
+    $(function() {
+        //se obtiene el token JWT desde el servidor
+        $.get('/app/token', function(token){
+            window.jwtToken = token;
+            angular.bootstrap(document, ['app']);
+            
+        });     
+             
+    });
+        
 })();
