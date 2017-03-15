7d401ba9b718f3149f613254daca6d8a3ae5daa6
Edward Steven Osorio Crispin <esosoriocrispin@gmail.com>
2017-03-16 00:55:05 +0100

    Actualizacion user.service.js (update funcion)

--- services/user.service.js
+++ services/user.service.js
@@ -85,3 +85,65 @@ function create(userParam) {
     }
     return deferred.promise;
 }
+
+function update(_id, userParam) {
+    var deferred = Q.defer();
+    
+    //validacion
+    db.users.findById(_id,function(err,user) {
+        if(err) deferred.reject(err);
+        
+        if (user.username !== userParam.username){
+            //username cambia si el nuevo username es tomado antes
+            db.users.findOne(
+                { username: userParam.username },
+                function(err,user){
+                    if(err) deferred.reject(err);
+                    
+                    if(user){
+                        //el username ya existe
+                        deferred.reject('Username "' +  req.body.username + '" ya esta siendo usado')
+                    }else{
+                        updateUser();
+                    }
+                });         
+        } else {
+            updateUser();
+        }
+    });
+    
+    function updateUser(){
+        //campos para actualizar
+        var set = {
+            firstName: userParam.firstName,
+            lastName: userParam.lastName
+            username: userParam.username,
+        };
+        
+        //actualizar password si fue introducido
+        if(userParam.password) {
+            set.hash = bcrypt.hashSync(userParam.password,10);
+        }
+        
+        db.users.update(
+            { _id: mongo.helper.toObjectID(_id)},
+            { $set: set },
+            function (err,doc) {
+                if(err) deferred.reject(err);
+                    deferred.resolve();
+            });
+    }
+    return deferred.promise;
+}
+
+//se llama asi a la funcion con subrayado ya que la palabra 'delete' es reservada por javascript
+function _delete(_id) {
+    var deferred = Q.defer();
+    db.users.remove(
+    { _id: mongo.helper.toObjectID(_id) },
+    function(err) {
+        if(err) deferred.reject(err);
+            deferred.resolve();
+    });
+    return deferred.promise;
+}
