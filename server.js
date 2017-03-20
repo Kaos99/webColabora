d06b3621d657b6109e4016efb261b6da8ce9b965
Edward Steven Osorio Crispin <esosoriocrispin@gmail.com>
2017-03-20 20:48:41 +0100

    Actualizacion del server.js

--- server.js
+++ server.js
@@ -5,28 +5,28 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
 var config = require('config.json');
-
+ 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
-
-//se usa JWT authentificacion para la seguridad del api
-app.use('/api',expressJwt({ secret: config.secret }).unless({path: ['/api/users/authenticate','api/users/register']}));
-
-//routes
-app.use('/login',require('./controllers/login.controller'));
+ 
+// use JWT auth to secure the api
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
+ 
+// routes
+app.use('/login', require('./controllers/login.controller'));
 app.use('/register', require('./controllers/register.controller'));
 app.use('/app', require('./controllers/app.controller'));
-app.use('/api/users', require('./controllers/api/users.controller'))
-
-//hacer /app la ruta por defecto
-app.get('/',function(req,res) {
-    return res.redirect('app');
+app.use('/api/users', require('./controllers/api/users.controller'));
+ 
+// make '/app' default route
+app.get('/', function (req, res) {
+    return res.redirect('/app');
 });
-
-//se inicia el servidor
-var server = app.listen(3000,function(){
-    console.log('Servidor atendiendo en http://' + server.address().address + ':' + server.address().port);
+ 
+// start server
+var server = app.listen(3000, function () {
+    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
 });
