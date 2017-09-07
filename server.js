f234fa8b0974c37c9516b13cf8e6538c1fa839e7
UB_EDU\edu.osorio <esosoriocrispin@gmail.com>
2017-09-07 13:05:40 +0200

    deploying project

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('rootpath')();
 var express = require('express');
+var RedisStore = require('connect-redis')(express);
 var app = express();
 var session = require('express-session');
 var bodyParser = require('body-parser');
